036ed8135216252e1eb0d3a0dbb795c3275d1d85
fadyashraf8 <fadyashraf909@gmail.com>
2024-06-28 03:08:02 +0300

    test4

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 
 export const dbConnection=()=>{
-    mongoose.connect(`mongodb://127.0.0.1:27017/e-commerce`).then(()=>{
+    mongoose.connect(`mongodb+srv://fady123:fady123@cluster0.9mgd5e3.mongodb.net/e-commerce-backend`).then(()=>{
         console.log("Connect Success!");
     }).catch(()=>{
         console.log("Connect Fail!");
